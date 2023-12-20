b74fcf60b3c4107366db4159389896bab47b1ca0
sebastian <sebastianalexis869@gmail.com>
2023-12-20 04:35:20 -0300

    refactorizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app.jsx';
 import './styles/index.css';
 
 const rootEl = document.getElementById('root');
